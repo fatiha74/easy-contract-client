af5692f23cd211a2bab6d4693e3099db716ca6ba
fatiha Abellaoui <fatiha.abdellaoui74@gmail.com>
2022-12-20 17:08:17 +0100

    maj contrat

--- src/app/components/contrat/contrat.component.ts
+++ src/app/components/contrat/contrat.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { DataService } from 'src/app/services/data.service';
-import { FormGroup } from '@angular/forms';
 import { Salarie } from 'src/app/models/salarie';
 
 @Component({
@@ -14,7 +14,9 @@ export class ContratComponent implements OnInit {
 salarieContrat!:any[]
   contratForm!: FormGroup
 
-  constructor(private _dataService:DataService) { }
+  constructor(
+    private _dataService:DataService,
+    private _fb:FormBuilder) { }
 
   ngOnInit(): void {
 
@@ -24,13 +26,29 @@ salarieContrat!:any[]
   // this.salarieContrat = response.salarie_id
   // salarie:{"salarie_id":response.salarie_id,"salarie_nom": response.nom}
   this.salarieContrat=response
-  console.warn(this.salarieContrat)
+
 
 })
 
+this.contratForm = this._fb.group({
+
+
+  salarieSelect: ["", Validators.required],
+ type_contrat: ["", Validators.required],
+  duree: ["", Validators.required],
+  date_debut: ["", Validators.required],
+  date_fin: ["", Validators.required],
+  periode: ["", Validators.required],
+  remuneration: ["", Validators.required],
+  motif: ["", Validators.required],
+
+
+})
+
+
   }
 onSubmit(){
-
+console.log(this.contratForm)
 }
 
 }
