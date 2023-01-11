38bfa8a1df52147e520f7048f0b510698231a6f1
fatiha Abellaoui <fatiha.abdellaoui74@gmail.com>
2023-01-11 17:08:30 +0100

    c

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'easy-contract',
+  webDir: 'dist/easy-contract',
+  bundledWebRuntime: false
+};
+
+export default config;
